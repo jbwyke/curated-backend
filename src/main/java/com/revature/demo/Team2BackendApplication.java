4d7a37dce3b46e66aba20de2b14eba8175bfcfc2
Ty Johnson <tdj182@gmail.com>
2021-10-01 13:55:46 -0500

    Pushing starter files

---
+++ src/main/java/com/revature/demo/Team2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.revature.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team2BackendApplication.class, args);
+	}
+
+}
