629a53b08b7a0e7b1499ee91edcb20142fe65406
Ty Johnson <tdj182@gmail.com>
2021-10-04 18:04:35 -0500

    db starting point copied from example

--- src/main/java/com/revature/ServletInitializer.java
+++ src/main/java/com/revature/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.revature.demo;
+package com.revature;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
