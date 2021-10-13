fe1c3e4b083d8b578ecec22866d334a7cd393478
Kirk Hahn <kirkhahn11@yahoo.com>
2021-10-13 17:11:40 -0500

    added one line to allow spring to communicate with angular

--- src/main/java/com/revature/security/SecurityConfigurer.java
+++ src/main/java/com/revature/security/SecurityConfigurer.java
@@ -54,7 +54,9 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/authenticate", "/register").permitAll().
+				.authorizeRequests().antMatchers("/authenticate")
+		        .permitAll().antMatchers(HttpMethod.OPTIONS, "/**")
+				.permitAll().
 				// all other requests need to be authenticated
 						anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
